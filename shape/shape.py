c1ba69e41e2b129ba7e7108a13bdff5a9c7291e6
Michael Obikwere <oobikwere@academic.rrc.ca>
2025-09-23 20:52:00 -0500

    Fifth Commit: Added required documentation of each methods.

--- shape/shape.py
+++ shape/shape.py
@@ -6,7 +6,22 @@ __version__ = "1.0.0"
 from abc import ABC, abstractmethod
 
 class Shape(ABC):
+    """
+    Shape super class: Maintains shape color.
+    """
+
     def __init__(self, color: str):
+
+        """
+        Initializes class attributes to corresponding argument value.
+
+        Args:
+            color (str): Represents the color of the Shape.
+
+        Raises:
+            ValueError: When color is blank.
+
+        """
 
         if len(color.strip()) > 0:
             self._color = color
@@ -14,15 +29,32 @@ class Shape(ABC):
             raise ValueError ("Color cannot be blank.")
         
     def __str__(self):
+
+        """
+        Returns a string representation of the shapes color.
+
+        Returns:
+            str: The color of the shape as a string.
+        """
         
         return (f"The shape color is {self._color}.")
     
     @abstractmethod
     def calculate_area() -> float:
 
+        """
+        Abstract method.
+        Implemented in subclass(es).
+        """
+
         pass
 
     @abstractmethod
     def calculate_perimeter() -> float:
 
+        """
+        Abstract method.
+        Implemented in subclass(es).
+        """
+        
         pass
