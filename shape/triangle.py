179135c5cada2f5700a70284283c0154fb593d7a
Michael Obikwere <oobikwere@academic.rrc.ca>
2025-09-24 11:33:35 -0500

    Sixth Commit: Initialises the init method for a subclass Triangle, with arguments, raised a value error when the arguments are not numeric and when it cant form a triangle.

--- shape/triangle.py
+++ shape/triangle.py
@@ -1,4 +1,38 @@
 """This module defines the Triangle class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Michael Obikwere"
+__version__ = "1.0.0"
+
+from shape.shape import Shape
+
+class Triangle(Shape):
+
+    def __init__(self, color: str, side_1: int, side_2: int,
+                  side_3: int):
+        
+        super().__init__(color)
+
+        if isinstance(side_1, int):
+            self.__side_1 = side_1
+        else:
+            raise ValueError("Side 1 must be numeric")
+
+        if isinstance(side_2, int):
+            self.__side_2 = side_2
+        else:
+            raise ValueError("Side 2 must be numeric")
+
+        if isinstance(side_3, int):
+            self.__side_3 = side_3
+        else:
+            raise ValueError("Side 3 must be numeric")
+
+        if (side_1 + side_2 > side_3 
+            and side_1 + side_3 > side_2 
+            and side_2 + side_3 > side_1):
+            
+            self.__side_1 = side_1
+            self.__side_2 = side_2
+            self.__side_3 = side_3
+        else:
+            raise ValueError("The sides do not satisfy the Triangle Inequality")
