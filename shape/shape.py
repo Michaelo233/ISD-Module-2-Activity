0a02df07e3e7f8737caa2a31fb5610dbffb5a39e
Michael Obikwere <oobikwere@academic.rrc.ca>
2025-09-23 20:01:53 -0500

    Second Commit: Initialised a init method with color as an attribute for a superclass shape, which raises value error for a vblank color.

--- shape/shape.py
+++ shape/shape.py
@@ -1,4 +1,14 @@
 """This module defines the Shape class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Michael Obikwere"
+__version__ = "1.0.0"
+
+from abc import ABC
+
+class Shape(ABC):
+    def __init__(self, color: str):
+
+        if len(color.strip()) > 0:
+            self._color = color
+        else:
+            raise ValueError ("Color cannot be blank.")
