6afda0e5036bab8e0a2430f065334ca009be05ba
Michael Obikwere <oobikwere@academic.rrc.ca>
2025-09-25 20:25:26 -0500

    Sixteenth Commit: Added required documentations where neccessary in the Rectangle class.

--- shape/rectangle.py
+++ shape/rectangle.py
@@ -6,9 +6,26 @@ __version__ = "1.0.0"
 from shape.shape import Shape
 
 class Rectangle(Shape):
-
+    """
+    Subclass Rectangle: Rectangle shape class.
+    """
     def __init__(self, color: str, length: int, width: int):
+        """
+        Initializes class attributes to corresponding argument value.
 
+        Args:
+            color (str): Represents the color of the Shape.
+            length (int): The length of 2 opposing sides of the 
+                rectangle.
+            width (int): The width of the two opposing sides of the 
+                rectangle.
+
+        Raises:
+            ValueError: When color is blank, When length is not an int,
+                When width is not an int.
+
+        """
+         
         super().__init__(color)
 
         if isinstance(length, int):
@@ -21,7 +38,15 @@ class Rectangle(Shape):
         else:
             raise ValueError("Width must be numeric.")
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Return string represention of the color, length and width of the
+            Rectangle.
+
+        Returns:
+            str: The color, length and width of each side of the 
+                Rectangle.
+        """
 
         shape_string = super().__str__()
         rectangle_string = (f"\nThis rectangle has four sides length of"
@@ -31,14 +56,27 @@ class Rectangle(Shape):
         
         return (shape_string + rectangle_string)
     
-    def calculate_area(self):
+    def calculate_area(self) -> float:
 
+        """
+        Implementing abstract method calculate area.
+
+        Returns:
+            float: Area of the rectangle as a float.
+        """
         area = self.__length * self.__width
 
-        return area
+        return float(area)
     
-    def calculate_perimeter(self):
+    def calculate_perimeter(self) -> float:
+
+        """
+        Implementing abstract method calculate perimeter.
+
+        Returns:
+            float: perimeter of the rectangle as a float.
+        """
 
         perimeter = (self.__length *2) + (self.__width * 2)
-        
-        return perimeter
+
+        return float(perimeter)
