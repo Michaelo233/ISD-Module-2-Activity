29e187fabefddbd4f8616043f2d5a73fa04793ae
Michael Obikwere <oobikwere@academic.rrc.ca>
2025-09-25 19:48:57 -0500

    Thirteeth Commit: Initialised init method for the rectangle sub class with required arguments.

--- shape/rectangle.py
+++ shape/rectangle.py
@@ -1,4 +1,22 @@
 """This module defines the Rectangle class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Michael Obikwere"
+__version__ = "1.0.0"
+
+from shape.shape import Shape
+
+class Rectangle(Shape):
+
+    def __init__(self, color: str, length: int, width: int):
+
+        super().__init__(color)
+
+        if isinstance(length, int):
+            self.__length = length
+        else:
+            raise ValueError("Length must be numeric.")
+        
+        if isinstance(width, int):
+            self.__width = width
+        else:
+            raise ValueError("Width must be numeric.")
